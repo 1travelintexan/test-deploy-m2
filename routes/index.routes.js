5fee94d166f965b69be4bdc57d63e464456f4da1
Joshua George <horheyinc8@gmail.com>
2023-11-21 18:15:32 +0100

    profile page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+router.get("/profile", (req, res) => {
+  console.log("sess profile page", req.session);
+  res.render("profile", { user: req.session.currentUser });
 });
 
 module.exports = router;
